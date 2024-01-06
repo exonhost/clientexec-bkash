f283e11d45a6e606a3fa11463b45018354b0feca
ExonHost <saleh@obosor.com>
2024-01-07 03:03:21 +0600

    Delete plugins/gateways/bkash-clientexec directory

--- plugins/gateways/bkash-clientexec/callback.php
+++
@@ -1,8 +0,0 @@
-<?php
-$_GET['fuse']='billing';
-$_GET['action'] = 'gatewaycallback';
-$_GET['plugin'] = 'bkash';
-
-chdir('../../..');
-
-require_once dirname(__FILE__).'/../../../library/front.php';
